f18a8f8d59540e00176ac55c74a13615c49cf496
David Banks <BanksySan@googlemail.com>
2023-04-25 22:48:29 +0100

    WebGL:  Simple Triangle.

--- web-gl/tutorial/simple-triangle-vertex-colours/app.js
+++ web-gl/tutorial/simple-triangle-vertex-colours/app.js
@@ -1,23 +1,24 @@
 const vertexShaderText = [
     'precision mediump float;',
-
     'attribute vec2 vertPosition;',
-
+    'attribute vec3 vertColor;',
+    'varying vec3 fragColor;',
     'void main() {',
+    '    fragColor = vertColor;',
     '    gl_Position = vec4(vertPosition, 0.0, 1.0);',
     '}',
 ].join('\n');
 
 const fragmentShaderText = [
     'precision mediump float;',
-
+    'varying vec3 fragColor;',
     'void main()',
     '{',
-    '    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);',
+    '    gl_FragColor = vec4(fragColor, 1.0);',
     '}',
 ].join('\n');
 
-const InitDemo = function () {
+var InitDemo = function () {
     const canvas = document.getElementById('game-surface');
     const gl = canvas.getContext('webgl');
     if (!gl) {
@@ -49,9 +50,10 @@ const InitDemo = function () {
             gl.getShaderInfoLog(fragmentShader),
     };
 
-    console.log('Compile status', compileStatus);
-
-    if (!(compileStatus.vertexStatus && compileStatus.fragmentStatus)) {
+    if (
+        compileStatus.vertexStatus !== true ||
+        compileStatus.fragmentStatus !== true
+    ) {
         console.error('Failed to compile.  Exiting.');
         return;
     }
@@ -68,7 +70,7 @@ const InitDemo = function () {
         gl.getProgramParameter(program, gl.LINK_STATUS) ||
         gl.getProgramInfoLog(program);
     console.log('Link status', linkingStatus);
-    if (!linkingStatus) {
+    if (linkingStatus !== true) {
         console.error('Linking failed.  Exiting.');
         return;
     }
@@ -79,19 +81,32 @@ const InitDemo = function () {
         gl.getProgramParameter(program, gl.VALIDATE_STATUS) ||
         gl.getProgramInfoLog(program);
     console.log('Validation status', validationStatus);
-    if (!validationStatus) {
+    if (validationStatus !== true) {
         console.error('Validation failed.  Exiting.');
         return;
     }
 
     console.log('Creating buffer');
 
+    const vertexTopCoordinates = [0.0, 0.5];
+    const vertexLeftCoordinates = [-0.5, -0.5];
+    const vertexRightCoordinates = [0.5, -0.5];
+
+    const vertexTopColor = [1.0, 0.0, 0.0];
+    const vertexLeftColor = [0.0, 1.0, 0.0];
+    const vertexRightColor = [0.0, 0.0, 1.0];
+
     /**
      * The <code>triangleVertices</code> needs to be a 32 bit array for WebGL.  JS arrays are 64 bit, thus the need to use the <code>Float32Array</code> constructor.
      * @type {Float32Array}
      */
     const triangleVertices = new Float32Array([
-        0.0, 0.5, -0.5, -0.5, 0.5, -0.5,
+        ...vertexTopCoordinates,
+        ...vertexTopColor,
+        ...vertexLeftCoordinates,
+        ...vertexLeftColor,
+        ...vertexRightCoordinates,
+        ...vertexRightColor,
     ]);
     const triangleVertexBufferObject = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject);
@@ -105,6 +120,10 @@ const InitDemo = function () {
         program,
         'vertPosition'
     );
+    const vertColorAttributeLocation = gl.getAttribLocation(
+        program,
+        'vertColor'
+    );
 
     /**
      * The <code>normalized</code> parameter here is listed in the video as <code>gl.FALSE</code>.
@@ -115,10 +134,21 @@ const InitDemo = function () {
         2,
         gl.FLOAT,
         false,
-        2 * Float32Array.BYTES_PER_ELEMENT,
+        5 * Float32Array.BYTES_PER_ELEMENT,
         0
     );
+
+    gl.vertexAttribPointer(
+        vertColorAttributeLocation,
+        3,
+        gl.FLOAT,
+        false,
+        5 * Float32Array.BYTES_PER_ELEMENT,
+        2 * Float32Array.BYTES_PER_ELEMENT
+    );
+
     gl.enableVertexAttribArray(positionAttributeLocation);
+    gl.enableVertexAttribArray(vertColorAttributeLocation);
 
     gl.useProgram(program);
     gl.drawArrays(gl.TRIANGLES, 0, 3);
