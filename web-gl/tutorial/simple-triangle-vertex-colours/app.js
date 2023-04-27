a5619ae2b2b87a5e669630dcd7144651aec55a8e
David Banks <BanksySan@googlemail.com>
2023-04-27 20:17:48 +0100

    Refactoring boilerplate into helper function.

--- web-gl/tutorial/simple-triangle-vertex-colours/app.js
+++ web-gl/tutorial/simple-triangle-vertex-colours/app.js
@@ -1,3 +1,7 @@
+import { createProgram } from '../../webglHelpers.js';
+
+export const FOO = 123;
+
 const vertexShaderText = [
     'precision mediump float;',
     'attribute vec2 vertPosition;',
@@ -18,73 +22,10 @@ const fragmentShaderText = [
     '}',
 ].join('\n');
 
-var InitDemo = function () {
+(function InitDemo() {
     const canvas = document.getElementById('game-surface');
     const gl = canvas.getContext('webgl');
-    if (!gl) {
-        console.error("This browser doesn't support WebGL");
-    }
-
-    gl.clearColor(0.75, 0.85, 0.8, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    console.log('Creating Shaders');
-
-    const vertexShader = gl.createShader(gl.VERTEX_SHADER);
-    const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-
-    console.log('Setting source');
-
-    gl.shaderSource(vertexShader, vertexShaderText);
-    gl.shaderSource(fragmentShader, fragmentShaderText);
-
-    gl.compileShader(vertexShader);
-    gl.compileShader(fragmentShader);
-
-    const compileStatus = {
-        vertexStatus:
-            gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS) ||
-            gl.getShaderInfoLog(vertexShader),
-        fragmentStatus:
-            gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS) ||
-            gl.getShaderInfoLog(fragmentShader),
-    };
-
-    if (
-        compileStatus.vertexStatus !== true ||
-        compileStatus.fragmentStatus !== true
-    ) {
-        console.error('Failed to compile.  Exiting.');
-        return;
-    }
-
-    console.log('Create program');
-    const program = gl.createProgram();
-
-    console.log('Attach shaders');
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
-    gl.linkProgram(program);
-
-    const linkingStatus =
-        gl.getProgramParameter(program, gl.LINK_STATUS) ||
-        gl.getProgramInfoLog(program);
-    console.log('Link status', linkingStatus);
-    if (linkingStatus !== true) {
-        console.error('Linking failed.  Exiting.');
-        return;
-    }
-
-    // The validating the program is expensive.  You don't do it in production code.
-    gl.validateProgram(program);
-    const validationStatus =
-        gl.getProgramParameter(program, gl.VALIDATE_STATUS) ||
-        gl.getProgramInfoLog(program);
-    console.log('Validation status', validationStatus);
-    if (validationStatus !== true) {
-        console.error('Validation failed.  Exiting.');
-        return;
-    }
+    const program = createProgram(gl, vertexShaderText, fragmentShaderText);
 
     console.log('Creating buffer');
 
@@ -152,4 +93,4 @@ var InitDemo = function () {
 
     gl.useProgram(program);
     gl.drawArrays(gl.TRIANGLES, 0, 3);
-};
+})();
--- web-gl/tutorial/simple-triangle/app.js
+++ web-gl/tutorial/simple-triangle/app.js
@@ -1,3 +1,5 @@
+import { createProgram } from '../../webglHelpers.js';
+
 const vertexShaderText = [
     'precision mediump float;',
 
@@ -17,7 +19,7 @@ const fragmentShaderText = [
     '}',
 ].join('\n');
 
-const InitDemo = function () {
+(function InitDemo() {
     const canvas = document.getElementById('game-surface');
     const gl = canvas.getContext('webgl');
     if (!gl) {
@@ -27,62 +29,7 @@ const InitDemo = function () {
     gl.clearColor(0.75, 0.85, 0.8, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    console.log('Creating Shaders');
-
-    const vertexShader = gl.createShader(gl.VERTEX_SHADER);
-    const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-
-    console.log('Setting source');
-
-    gl.shaderSource(vertexShader, vertexShaderText);
-    gl.shaderSource(fragmentShader, fragmentShaderText);
-
-    gl.compileShader(vertexShader);
-    gl.compileShader(fragmentShader);
-
-    const compileStatus = {
-        vertexStatus:
-            gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS) ||
-            gl.getShaderInfoLog(vertexShader),
-        fragmentStatus:
-            gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS) ||
-            gl.getShaderInfoLog(fragmentShader),
-    };
-
-    console.log('Compile status', compileStatus);
-
-    if (!(compileStatus.vertexStatus && compileStatus.fragmentStatus)) {
-        console.error('Failed to compile.  Exiting.');
-        return;
-    }
-
-    console.log('Create program');
-    const program = gl.createProgram();
-
-    console.log('Attach shaders');
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
-    gl.linkProgram(program);
-
-    const linkingStatus =
-        gl.getProgramParameter(program, gl.LINK_STATUS) ||
-        gl.getProgramInfoLog(program);
-    console.log('Link status', linkingStatus);
-    if (!linkingStatus) {
-        console.error('Linking failed.  Exiting.');
-        return;
-    }
-
-    // The validating the program is expensive.  You don't do it in production code.
-    gl.validateProgram(program);
-    const validationStatus =
-        gl.getProgramParameter(program, gl.VALIDATE_STATUS) ||
-        gl.getProgramInfoLog(program);
-    console.log('Validation status', validationStatus);
-    if (!validationStatus) {
-        console.error('Validation failed.  Exiting.');
-        return;
-    }
+    const program = createProgram(gl, vertexShaderText, fragmentShaderText);
 
     console.log('Creating buffer');
 
@@ -122,4 +69,4 @@ const InitDemo = function () {
 
     gl.useProgram(program);
     gl.drawArrays(gl.TRIANGLES, 0, 3);
-};
+})();
