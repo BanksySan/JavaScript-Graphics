b763fc5fd22f4a05f4c95de1b2376e5e69bcf4ed
David Banks <BanksySan@googlemail.com>
2023-05-19 20:52:00 +0100

    WebGL: Hello World template.

---
+++ web-gl/hello-world/hello-world.js
@@ -0,0 +1,14 @@
+import { createProgram, fetchShaderTexts, initialiseMesh } from '../../helpers/webglHelpers.js';
+
+const SHADER_TEXTS_PROMISE = fetchShaderTexts('./hello-world.vert.glsl', './hello-world.frag.glsl');
+const CANVAS = document.getElementById('webgl-hello-world');
+const gl = CANVAS.getContext('webgl2');
+const SHADER_TEXTS = await SHADER_TEXTS_PROMISE;
+const program = createProgram(gl, SHADER_TEXTS.vertexShaderText, SHADER_TEXTS.fragmentShaderText);
+
+gl.useProgram(program);
+
+initialiseMesh(program, gl, 'VERTEX_POSITION');
+
+gl.uniform2f(gl.getUniformLocation(program, 'CANVAS_SIZE'), CANVAS.width, CANVAS.height);
+gl.drawArrays(gl.TRIANGLES, 0, 6);
---
+++ web-gl/hello-world/hello-world.vert.glsl
@@ -0,0 +1,7 @@
+precision mediump float;
+
+attribute vec2 VERTEX_POSITION;
+
+void main() {
+    gl_Position = vec4(VERTEX_POSITION, 0.0, 1.0);
+}
